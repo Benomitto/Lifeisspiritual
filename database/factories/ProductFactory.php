847255cf71e80796be66532cd69a0943afe06f38
Benomitto <benjaminomitto@gmail.com>
2021-09-30 19:30:33 +0300

    'ProductsUpdate'

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -24,13 +24,13 @@ class ProductFactory extends Factory
     {
         return [
             //
-	"title"=>$this->faker->sentence,
-	"slug"=>Str::slug($this->faker->sentence),
-	"description"=>$this->faker->paragraph,
-	"price"=>$this->faker->numberBetween($min=100,$max=900),
-	"old_price"=>$this->faker->numberBetween($min=100,$max=900),
-	"inStock"=>$this->faker->numberBetween($min=1,$height=10),
-	"image"=>$this->faker->imageUrl($width=640,$height=480),
+	"title"=>$this->sentence,
+	"slug"=>Str::slug($this->sentence),
+	"description"=>$this->paragraph,
+	"price"=>$this->numberBetween($min=100,$max=900),
+	"old_price"=>$this->numberBetween($min=100,$max=900),
+	"inStock"=>$this->numberBetween($min=1,$height=10),
+	"image"=>$this->imageUrl($width=640,$height=480),
 	"category_id"=>Category::factory()->create()->id,
         ];
     }
